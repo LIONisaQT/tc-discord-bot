e9781d6f04615d36dc4ed4c82be0b40ead5a3752
Ryan Shee <7772995+LIONisaQT@users.noreply.github.com>
2024-12-21 15:15:19 -0800

    Add TypeScript, add simple ping command

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import { Client, Events } from "discord.js";
+import { config } from "./config";
+import { commands } from "./commands";
+import { deployCommands } from "./deploy-commands";
+
+const client = new Client({
+  intents: ["Guilds", "GuildMessages", "DirectMessages"],
+});
+
+client.once(Events.ClientReady, (readyClient: Client<true>) => {
+	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
+
+client.on("guildCreate", async (guild) => {
+  await deployCommands({ guildId: guild.id });
+});
+
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isCommand()) {
+    return;
+  }
+  const { commandName } = interaction;
+  if (commands[commandName as keyof typeof commands]) {
+    commands[commandName as keyof typeof commands].execute(interaction);
+  }
+});
+
+client.login(config.DISCORD_TOKEN);
+
